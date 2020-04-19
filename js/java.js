bb1d9eb8ee95ca160f5dee3178b4be438d46047c
Duneya <29269@ma-web.nl>
2020-04-20 01:41:07 +0200

    versie2

--- js/java.js
+++ js/java.js
@@ -1,3 +1,4 @@
+//eye voor wachtwoord...
 function viewPassword() {
     var passwordInput = document.getElementById('password-field');
     var passStatus = document.getElementById('pass-status');
@@ -10,4 +11,22 @@ function viewPassword() {
         passwordInput.type = 'password';
         passStatus.className = 'fa fa-eye';
     }
+}
+// switch
+const vlakken = document.querySelectorAll('.vlak');
+const rimpel = document.createElement('div');
+rimpel.classList = 'rimpel';
+
+
+function maakRipple(e) {
+    console.log(e);
+    let x = e.pageX - this.offsetLeft;
+    let y = e.pagey - this.offsetTop;
+    rimpel.style.left = x + 'px';
+    rimpel.style.top = y + 'px';
+    e.target.append(rimpel);
+}
+
+for (let i = 0; i < vlakken.length; i++) {
+    vlakken[i].addEventListener('click', maakRipple);
 }
